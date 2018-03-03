ba6dc4e4ac8676a5598fbcdaf1d908c2a53414fc
Pratik Murhe <prtk789@gmail.com>
2018-03-03 14:18:07 +0530

    added base-64 encoding

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,15 @@
 
-var express = require('express');
-var app     = express();
-var router = express.Router();
-var  db = require('../dbConnection.js');
-
+const express = require('express');
+const app     = express();
+const router = express.Router();
+const  db = require('../dbConnection.js');
+const base64 = require('base-64');
 
 router.post('/login', function(req, res) {
 	var user_name  = req.body.user_name;
 	var password = req.body.password;
-
+	password = base64.encode(password);
+	
 	var query = "select * from user_master where user_name = '" + user_name + "' and user_password = '"+password+"'";
 	console.log(query);
 
