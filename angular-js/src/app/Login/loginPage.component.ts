0556dc957310debd13c44ae58ed2682b6079cbb1
CIS\PSalunke <PSalunke@chenoainc.com>
2018-03-31 19:47:36 +0530

    routing and data fetching as well as api call

--- angular-js/src/app/Login/loginPage.component.ts
+++ angular-js/src/app/Login/loginPage.component.ts
@@ -16,36 +16,31 @@ import { AuthService } from '../shared/guards/auth.service';
 })
 export class LoginPageComponent {
   private inputData: any = {};
-  private url: string = 'api/authenticate';
-  public data = "";
+  private url: string = 'login';
+  public responseData = "";
   //public url: string = 'api/users';
 
   constructor(private webApiService: WebApiService, private _router: Router,
-    private datePipe: DatePipe,private authService: AuthService) { 
-        
-    }
+    private datePipe: DatePipe, private authService: AuthService) {
+
+  }
 
 
   onSubmit(loginForm: NgForm) {
-    debugger
     this.authService.setIsOnline(true);
     this.inputData = loginForm.form.value;
-    //this.webApiService.saveData(this.inputData, this.url).subscribe(resp => this.ResponseData(resp));
-    if (this.inputData) {
-      this._router.navigate(['/ControllerDashboard']);
-    } 
+    this.webApiService.saveData(this.inputData, this.url).subscribe(resp => this.ResponseData(resp));
   }
   /**
    * ResponseData to set patient values in local storage
    */
   public ResponseData(Data: any) {
     debugger
-    //this.loginResponse = Data;
-    //console.log(this.loginResponse);
-    if (this.inputData) {
-      this._router.navigate(['/Dashboard1']);
+    this.responseData = Data.result[0];
+    if (this.responseData) {
+      this._router.navigate(['/ControllerDashboard']);
     } else {
-      this.data = "Incorrect password or username";
+      this.responseData = "Incorrect password or username";
     }
   }
 }
